fe9e7591c8c8ee00313a600699746a93de8849cc
Schuyler Meyer <31451505+schuybreach@users.noreply.github.com>
2018-04-09 16:55:31 -0400

    added stuff
    added abs_layout.xml and 2 lines in main_activity
    -Schuyler

--- app/src/main/java/com/clarkson/connect/clarksonconnect/MainActivity.java
+++ app/src/main/java/com/clarkson/connect/clarksonconnect/MainActivity.java
@@ -1,6 +1,7 @@
 package com.clarkson.connect.clarksonconnect;
 
 import android.graphics.Color;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -20,6 +21,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setCustomView(R.layout.abs_layout);
 
         //OBJECT DECLARATIONS
         ptt = (Button) findViewById(R.id.PTT);
