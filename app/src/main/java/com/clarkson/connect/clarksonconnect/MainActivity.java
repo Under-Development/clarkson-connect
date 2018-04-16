a9c6103930aa4bd5afc197afca12f310216660d2
Schuyler Meyer <31451505+schuybreach@users.noreply.github.com>
2018-04-15 22:46:23 -0400

    updated constraints
    ...to make them more automatic. (they were all over the place on my phone.) I still cant make the action bar transparent though.

--- app/src/main/java/com/clarkson/connect/clarksonconnect/MainActivity.java
+++ app/src/main/java/com/clarkson/connect/clarksonconnect/MainActivity.java
@@ -26,9 +26,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //---------- ALL FOR THE ACTION BAR ----------//
         // to center the title bar (...they call it the action bar)
         getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
         getSupportActionBar().setCustomView(R.layout.abs_layout);
+
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+
+        //---------- END ACTION BAR ----------//
 
         //OBJECT DECLARATIONS
         ptt = (Button) findViewById(R.id.PTT);
