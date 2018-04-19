ff70d046e8563ee00b10b3051e5424a1502d63af
wcoleman39 <coleman.wi.j@gmail.com>
2018-04-19 17:31:40 -0400

    Add separate call functions

--- app/src/main/java/com/clarkson/connect/clarksonconnect/FoodCallDialogFragment.java
+++ app/src/main/java/com/clarkson/connect/clarksonconnect/FoodCallDialogFragment.java
@@ -5,20 +5,16 @@ import android.app.Dialog;
 import android.app.DialogFragment;
 import android.content.DialogInterface;
 import android.os.Bundle;
-import android.content.Intent;
-import android.net.Uri;
 
-public class CallDialogFragment extends DialogFragment {
+public class FoodCallDialogFragment extends DialogFragment {
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         // Use the Builder class for convenient dialog construction
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        final Intent phoneIntent = new Intent(Intent.ACTION_CALL);
-        builder.setMessage(R.string.dialog_call_security)
+        builder.setMessage(R.string.dialog_call_food)
                 .setPositiveButton(R.string.confirm, new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
                         //Open the Phone app and dial the number
-                        phoneIntent.setData(Uri.parse("tel:1-315-265-2031"));
                     }
                 })
                 .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
